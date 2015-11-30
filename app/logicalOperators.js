96395e59b33ae64a679153d8e27092ebf8716e2c
Elizabeth Banalagay <zbanalagay@gmail.com>
2015-11-30 09:34:39 -0800

    finished logical operators

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -2,10 +2,11 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.logicalOperatorsAnswers = {
   or : function(a, b) {
+    return a||b;
 
   },
 
   and : function(a, b) {
-
+    return a && b;
   }
 };
